e9ed933e7d12d90dfa750539aefc7d465de17429
jordynrmackie <67487854+jordynrmackie@users.noreply.github.com>
2020-06-27 12:14:39 +1200

    Update PULSE.py

--- PULSE.py
+++ PULSE.py
@@ -34,7 +34,7 @@ class PULSE(torch.nn.Module):
             if self.verbose: print("\tLoading Mapping Network")
             mapping = G_mapping().cuda()
 
-            with open_url("https://drive.google.com/uc?id=14R6iHGf5iuVx3DMNsACAl7eBr7Vdpd0k", cache_dir=cache_dir, verbose=verbose) as f:
+            with open_url("https://drive.google.com/file/d/16YIeW-dr_XJWyHGEEL-yXx3Zif0WgCuB/view?usp=sharing", cache_dir=cache_dir, verbose=verbose) as f:
                     mapping.load_state_dict(torch.load(f))
 
             if self.verbose: print("\tRunning Mapping Network")
